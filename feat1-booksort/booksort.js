e1a990b2ede36c2b64be619df8b6c922aa381adf
bsimm023 <bsimm023@fiu.edu>
2021-09-27 21:39:39 -0400

    updates

--- feat1-booksort/booksort.js
+++ feat1-booksort/booksort.js
@@ -1,6 +1,10 @@
-function search() {
-    //Find text entered from search nox through SQL query
-    
+function search(criteria) {
+    //Find text entered from search box through SQL query
+    var pg = require(‘pg’);
+    var connectionString = "postgres://postgres:Skwip2@(0)@localhost/127.0.0.1:5432/BookStore";
+    var pgClient = new pg.Client(connectionString);
+    pgClient.connect();
+    var query = pgClient.query("SELECT Title FROM book WHERE Title like " + criteria);
 };
 
 function readTextFile(file)
