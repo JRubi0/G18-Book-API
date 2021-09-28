968cf7a596c900454c0e70604d18301c06ace345
bsimm023 <bsimm023@fiu.edu>
2021-09-27 21:39:28 -0400

    Updates

--- feat1-booksort/booksort.js
+++ feat1-booksort/booksort.js
@@ -1,3 +1,22 @@
-search(); {
+function search() {
+    //Find text entered from search nox through SQL query
+    
+};
 
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
 }
