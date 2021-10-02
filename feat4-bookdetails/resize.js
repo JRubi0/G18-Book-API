d79215c4e9456a593161a826bcfe55ff2f7676be
JRubi0 <jrubi110@fiu.edu>
2021-10-02 17:13:50 -0400

    added jquery UI plugin

--- feat4-bookdetails/resize.js
+++ feat4-bookdetails/resize.js
@@ -13,4 +13,14 @@ jQuery(document).ready(function($){
       $('#second-half').detach().insertAfter('#first-half');
     }
     });
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].onclick = function(){
+          this.classList.toggle("active");
+          this.nextElementSibling.classList.toggle("show");
+    }
+}
   });
