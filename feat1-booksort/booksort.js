b687ef4c732605da74407ee9638665f9b0754d0c
JRubi0 <jrubi110@fiu.edu>
2021-10-02 18:18:26 -0400

    adjusted feature 4 js files

--- feat1-booksort/booksort.js
+++ feat1-booksort/booksort.js
@@ -1,6 +1,6 @@
 function search(criteria) {
     //Find text entered from search box through SQL query
-    var pg = require(‘pg’);
+    var pg = require(pg);
     var connectionString = "postgres://postgres:Skwip2@(0)@localhost/127.0.0.1:5432/BookStore";
     var pgClient = new pg.Client(connectionString);
     pgClient.connect();
