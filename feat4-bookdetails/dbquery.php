3539ba982c552a7b9db3b1c47af60d91183dc2c5
JRubi0 <jrubi110@fiu.edu>
2021-10-13 02:27:30 -0400

    testing SQL queries

---
+++ feat4-bookdetails/dbquery.php
@@ -0,0 +1,8 @@
+<?php
+    $servername = "archegos.ddns.net";
+    $username = "juanrubio";
+    $password = "Group18";
+    $dbname = "BookStore";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname)
+?>
