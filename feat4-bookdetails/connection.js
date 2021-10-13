f3feddab0ae6350a363250b078b5dd01560b4360
JRubi0 <jrubi110@fiu.edu>
2021-10-13 05:37:21 -0400

    testing queries, express.js added

---
+++ feat4-bookdetails/connection.js
@@ -0,0 +1,11 @@
+const {Client} = require('pg')
+
+const client = new Client({
+    host: "archegos.ddns.net",
+    user: "juanrubio",
+    port: 5432,
+    password: "Group18",
+    database: "Bookstore"
+})
+
+module.exports = client
--- feat4-bookdetails/dbquery.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "archegos.ddns.net";
-    $username = "juanrubio";
-    $password = "Group18";
-    $dbname = "BookStore";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname)
-?>
