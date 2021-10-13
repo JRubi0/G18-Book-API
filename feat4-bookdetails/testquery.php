c64ef8a89ef1e96b7379906f2d394f5d6f06f404
JRubi0 <jrubi110@fiu.edu>
2021-10-13 02:44:18 -0400

    testing queries

--- feat4-bookdetails/testquery.php
+++ feat4-bookdetails/testquery.php
@@ -16,16 +16,18 @@
         <body>
             <div id="comments">
                 <?php
-                    $sql = "SELECT * FROM book LIMIT 2;"
+                    $sql = "SELECT * FROM book LIMIT 2";
                     $result = mysqli_query($conn, $sql);
-                    if (mysqli_num_rows() > 0)
+                    if (mysqli_num_rows($result) > 0)
                     {
                         while ($row = mysqli_fetch_assoc($result))
-                        echo "<p>";
-                        echo $row['book_id'];
-                        echo "<br>";
-                        echo $row['title'];
-                        echo "</p>";
+                        {
+                            echo "<p>";
+                            echo $row['book_id'];
+                            echo "<br>";
+                            echo $row['title'];
+                            echo "</p>";
+                        }
                     }
                     
                     else
