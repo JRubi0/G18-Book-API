7d18fafb37c989a309325b01018eff84f855fa71
bsimm023 <bsimm023@fiu.edu>
2021-10-20 16:05:48 -0400

    Sort update

--- search/f1helloworld.php
+++ search/f1helloworld.php
@@ -5,51 +5,68 @@
 
 <!DOCTYPE HTML>
 <html>
-<head>
-    <title>G18 Homepage</title>
-    <link rel = "stylesheet" type = "text/css" href = "/css/globalFormat.css">
-    <link href = "https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,900" rel="stylesheet">
-    <script src="../scripts/jquery.js"></script>  <!--Includes JQuery-->
-</head>
+    <head>
+        <title>G18 Homepage</title>
+        <link rel = "stylesheet" type = "text/css" href = 'Format.css'>
+        <link href = "https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,900" rel="stylesheet">
+        <script src="../scripts/jquery.js"></script>  <!--Includes JQuery-->
+    </head>
 
-<body>
-    <header> 
-        <div class = "container">
-            <img src = '../assets/logo.png' alt = "logo" class = "logo"> <!--FIX ME-->
-            <nav>
-                <ul>
-                    <li><a href="../index.php">Home</a></li> <!--replace # with other pages-->
-                    <li><a href="#">Search</a></li>
-                    <li><a href="#">Wishlist</a></li>
-                    <li><a href="#">Profile</a></li>
-                    <li><a href="cart/shoppingcart.php">Checkout</a></li>
-                </ul>
-            </nav>
-        </div>     
-    </header>
+    <body>
+        <header> 
+            <div class = "container">
+                <img src = '../assets/logo.png' alt = "logo" class = "logo"> <!--FIX ME-->
+                <nav>
+                    <ul>
+                        <li><a href="../index.php">Home</a></li> <!--replace # with other pages-->
+                        <li><a href="#">Search</a></li>
+                        <li><a href="#">Wishlist</a></li>
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="cart/shoppingcart.php">Checkout</a></li>
+                    </ul>
+                </nav>
+            </div>     
+        </header>
 
-<body>
-  <div class = "SearchResults">
-          <div class = "SearchHeader">
-              <p><a href="search/f1helloworld.php?ORDER_BY=Author">Order by Author</a></p>
-              <p><a href="search/f1helloworld.php?ORDER_BY=Genre">Order by Genre</a></p>
-              <p><a href="search/f1helloworld.php?ORDER_BY=Rating">Order by Rating</a></p>
-              <input type="text" placeholder="What are you looking for?" id="criteria" name="criteria" value="criteria">
-              <a href="search/f1helloworld.php?WHERE_Author=" onclick="window.location=this.href+document.getElementById('criteria').value;return false;">Search Author</a>  
-              <a href="search/f1helloworld.php?WHERE_Title=" onclick="window.location=this.href+document.getElementById('criteria').value;return false;">Search Titles</a>  
-          </div>    
-  </div>
-
-  <?php
-  $Order_by = $_GET['ORDER_BY']; //This reads the ?book= parameter from the URL
-  $book_Author = $_GET['WHERE_Author']; //This reads the ?book= parameter from the URL
-  $title = $_GET['title']; //This reads the ?book= parameter from the URL
-  ?>
-
-
-</body>
-
-
-
-
+    <body>
+        <div class = "ShoppingCartContainer">
+            <div class = "CartHeader">
+                <p>
+                    <a href="f1helloworld.php?ORDER_BY=author_name">Order by Author</a>
+                </p>
+                <p>
+                    <a href="f1helloworld.php?ORDER_BY=Title">Order by Title</a>
+                </p>
+                <p>
+                    <a href="f1helloworld.php?ORDER_BY=rating">Order by Rating</a>
+                </p>
+                <input type="text" placeholder="What are you looking for?" id="criteria" name="criteria" value="criteria">
+                <a href="f1helloworld.php?WHERE_Author=" onclick="window.location=this.href+document.getElementById('criteria').value;return false;">Search Author</a>  
+                <a href="f1helloworld.php?WHERE_Title=" onclick="window.location=this.href+document.getElementById('criteria').value;return false;">Search Titles</a>  
+            </div>   
+            <?php
+            echo "<html>
+                <body>
+                <table>
+                    <tr>
+                        <th>Book ID</th>
+                        <th>Name</th>
+                        <th>Genre</th>
+                        <th>Author</th>
+                        <th>Rating</th>
+                    </tr>";
+            $argc = $_GET['ORDER_BY'];
+            $result = pg_query($con, " SELECT book.book_id, title, genre, author_name FROM book, author, book_author WHERE book.book_id = book_author.book_id AND author.author_id = book_author.author_id ORDER BY $argc"); 
+            while ($row = pg_fetch_assoc($result) ){
+                echo   "<tr>
+                            <td>".$row['book_id']."</td>
+                            <td>".$row['title']."</td>
+                            <td>".$row['genre']."</td>
+                            <td>".$row['author_name']."</td>
+                        </tr>";
+                }
+                echo  "</table></body></htmml>";
+            ?> 
+        </div>
+    </body>
 </html>
