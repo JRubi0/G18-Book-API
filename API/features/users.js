04f0fced6eced48af0fffa4f0e4bf6488ea2a996
starlags <sschw073@fiu.edu>
2021-10-27 01:42:07 -0700

    added user template

---
+++ API/features/users.js
@@ -0,0 +1,17 @@
+// Not active yet also this top section will be in the index.js file later.
+// Profile Management routes that uses the db object for handling queries
+// app.post('/customer/new'), db.createNewUser)           // call to create new customers
+// app.get('customer/:id', db.getUserByEmail)             // call to retrieve customer by email (ADMIN?)
+// app.put('/customer/details', db.updateUserDetails)     // call to update customer details
+// app.post('/customer/payment', db.addCreditCard)        // call to add credit cards
+// app.get('/customer/payment', db.getCreditCard)         // call to pull all cards on account
+
+
+// Methods need to be created for all these
+module.exports = {
+    //createNewUser,
+    //getUserByEmail,
+    //updateUserDetails,
+    //addCreditCard,
+    //getCreditCar
+  }
