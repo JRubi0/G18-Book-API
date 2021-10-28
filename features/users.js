2f8df600760c95a1a721268e4074a0d86ddf105e
bsimm023 <89810768+bsimm023@users.noreply.github.com>
2021-10-28 14:41:47 -0400

    Added queries for cart and users

--- features/users.js
+++ features/users.js
@@ -6,6 +6,28 @@
 // app.post('/customer/payment', db.addCreditCard)        // call to add credit cards
 // app.get('/customer/payment', db.getCreditCard)         // call to pull all cards on account
 
+/*
+//I wrote the preliminary queries for these requirements  --Bryan
+//Create user
+	INSERT INT customer (username, password, name, email, home_address) VALUES
+	($username, $password, $name, $email, $home_address);
+
+//Get user
+	SELECT * FROM customer WHERE username LIKE "$username";
+
+//Update user
+	UPDATE customer SET $field = $newValue WHERE username = $username;
+
+//Add credit card
+	INSERT INTO credit_card (card_number, exp_date, code, email) VALUES
+	($card_number, $exp_date, $code, $email);
+
+//Get credit cards
+  SELECT * FROM credit_card WHERE email LIKE "$email";
+
+*/
+
+
 
 // Methods need to be created for all these
 module.exports = {
